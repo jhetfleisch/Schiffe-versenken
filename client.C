0e64733fdf187ad80302befc0d1e2c578f6f88c9
Ubuntu_16.04 <niko2000lau@gmail.com>
2024-01-17 14:03:17 +0100

    Strategie jdflg implememtiert

--- client.C
+++ client.C
@@ -27,7 +27,7 @@ int zfl (TCPclient *c ) {                                // Strategie: Feld wird
     s << "NEUEWELT"<<  endl;                            // Eine neue Welt wird angefordert
     c->sendData(s.str());
     msg = c->receive(32);
-    //cout << msg << endl;
+    cout << msg << endl;
 
     do{
 
@@ -42,20 +42,108 @@ int zfl (TCPclient *c ) {                                // Strategie: Feld wird
         c->sendData(msg.c_str());
         z1++;                                          // Anzahl der der Durchläufe wird hier um 1 hochgezählt
         msg = c->receive(32);                          // empfangener String vom Server, überschreibt den String msg
-        /*cout << "got response" << msg << endl;*/
+        cout << "got response" << msg << endl;
         sscanf(msg.c_str(), "RESULT[%i]", &r);                 // aus dem String des Servers wird die Variable r ausgelesn und in eine Integer Variable umgewandelt
 
     }while (r != 4);
 
     cout << z1 << endl;
-    /*cout << "Alle Schiffe wurden versenkt" << endl;*/
+    //cout << "Alle Schiffe wurden versenkt" << endl;
     return (0);
 
+}
+
+/*int jdzwfld   (TCPclient *c){       // Strategie: Jedes zweite Feld soll beschossen werden. Bei einem Treffer werden die Felder davor und danach beschossen
+
+        int x = 1;
+        int y = 1;
+        int z2 = 0;
+        int r;
+        stringstream s;
+        string msg;
+
+        s << "NEUEWELT"<<  endl;                            // Eine neue Welt wird angefordert
+        c->sendData(s.str());
+        msg = c->receive(32);
 
 
+        for (x = 1; x < 10; x+2){
 
+            stringstream s;
+
+            s << "SHOOT[" << x << "," << y << "]";
+            msg = s.str ();
+            c->sendData(msg.c_str());
+            z2++;                                          // Anzahl der der Durchläufe wird hier um 1 hochgezählt
+            msg = c->receive(32);
+            sscanf(msg.c_str(), "RESULT[%i]", &r);
+
+            x = (x+2) %10+1;
+
+
+            if (x=10){
+                y++;
+            }
+
+            if (r = 1){
+                s << "SHOOT[" << x-1 << "," << y << "]";
+                msg = s.str ();
+                c->sendData(msg.c_str());
+                z2++;                                          // Anzahl der der Durchläufe wird hier um 1 hochgezählt
+                msg = c->receive(32);
+                s << "SHOOT[" << x+1 << "," << y << "]";
+                msg = s.str ();
+                c->sendData(msg.c_str());
+                z2++;                                          // Anzahl der der Durchläufe wird hier um 1 hochgezählt
+                msg = c->receive(32);
+            }
+
+            if (r = 4 ){
+                cout << z2 << endl;
+                return 0;
+            }
+        }
+
+}*/
+
+int jdfld (TCPclient *c ){                              // Strategie: Alle Felder werden nacheinander beschossen.
+
+    int x = 1;
+    int y = 1;
+    int z3 = 0;                                         // Durchlaufzähler
+    int r;
+
+    stringstream s;
+    string msg;
+
+    s << "NEUEWELT"<<  endl;                            // Eine neue Welt wird angefordert
+    c->sendData(s.str());
+    msg = c->receive(32);
+    cout << msg << endl;
+
+
+    for ( x=1; x<=10; x++){                                 // Aüßere Schleife zum hochzählen der x-Parameter
+        for (y=1; y<=10; y++){                              // Innere Schleife zum Durchlaufen der y-Parameter
+
+                stringstream s;
+
+                s << "SHOOT[" << x << "," << y << "]";
+                msg = s.str ();
+                c->sendData(msg.c_str());
+                z3++;                                          // Anzahl der der Durchläufe wird hier um 1 hochgezählt
+                msg = c->receive(32);
+                sscanf(msg.c_str(), "RESULT[%i]", &r);
+
+                if (r ==4){
+                cout << z3 << endl;
+                return z3;
+                }
+
+        }
+    }
 
 }
+
 
 int main() {
 	srand(time(NULL));
@@ -65,10 +153,12 @@ int main() {
 
 	//connect to host
 	c.conn(host , 2022);
-
-    for (int x=0; x<101; x++){
+    jdfld (&c);
+    //zfl (&c);
+	//jdzwfld(&c);
+    /*for (int x=0; x<101; x++){
     zfl (&c);
-    }
+    }*/
     return 0;
 }
 
